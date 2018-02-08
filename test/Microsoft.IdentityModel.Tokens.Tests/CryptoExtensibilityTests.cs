3f813958208dad2aaf5d10f9ecdf6bc1e06907c1
BrentSchmaltz <brentschmaltz@hotmail.com>
2018-02-07 20:10:32 -0800

    ifdef namespace

--- test/Microsoft.IdentityModel.Tokens.Tests/CryptoExtensibilityTests.cs
+++ test/Microsoft.IdentityModel.Tokens.Tests/CryptoExtensibilityTests.cs
@@ -27,7 +27,11 @@
 
 using System;
 using System.Globalization;
+#if System
 using System.IdentityModel.Tokens.Jwt;
+#else
+using Microsoft.IdentityModel.Tokens.Jwt;
+#endif
 using System.Text;
 using Microsoft.IdentityModel.Tests;
 using Xunit;
